384ba2294924cd5882ee1dca09bb86e39bef55fe
pawan-swqa <pawan.darji@swqa-solutions.com>
2022-07-12 16:42:49 +0530

    minor fix in reducer

--- src/state/reducers/wheatherReducer.ts
+++ src/state/reducers/wheatherReducer.ts
@@ -4,7 +4,10 @@ import {Action} from "../actions/index"
 const reducer = (state:any = {} , action:Action) => {
     switch (action.type) {
         case WheatherActionType.GET_WHEATHER:
-            return {wheatherData:action.data};
+            return {
+                ...state,
+                wheatherData:action.data
+            };
         default:
             return state
     }
