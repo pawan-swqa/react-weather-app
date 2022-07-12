a22ef1e0f7114df5bd328250dbd8451eebf88e4f
pawan-swqa <pawan.darji@swqa-solutions.com>
2022-07-12 15:54:01 +0530

    removing consoles

--- src/state/action-creators/wheather-actions.ts
+++ src/state/action-creators/wheather-actions.ts
@@ -4,7 +4,6 @@ import {Action} from "../actions/index"
 import wheatherService from "../../shared-services/wheather-service";
 
 export const getWheather = (url:string) => {
-    console.log("called")
     return async (dispatch: Dispatch<Action>) => {
         await wheatherService.getWheatherData(url).subscribe((res) => {
             dispatch({
